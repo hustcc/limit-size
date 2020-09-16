6048b291b160fb2da5a0ed463bfaab27abf9358d
hustcc <i@hust.cc>
2020-09-16 09:34:53 +0800

    chore: use byte-parser

--- __tests__/parse-size.spec.ts
+++ __tests__/parse-size.spec.ts
@@ -1,27 +1,6 @@
 import { parse, format } from '../src/bytes';
 
 describe('parse-size', () => {
-  test('parse', () => {
-    expect(parse('100')).toBe(100);
-    expect(parse('100b')).toBe(100);
-    expect(parse('100 b')).toBe(100);
-    expect(parse('10.1 b')).toBe(10.1);
-
-    expect(parse('10.1 kb')).toBe(10.1 * 1024);
-    expect(parse('10.1 KB')).toBe(10.1 * 1024);
-
-    expect(parse('1.2 mb')).toBe(1.2 * 1024 * 1024);
-    expect(parse('1.2   mb')).toBe(1.2 * 1024 * 1024);
-
-    expect(parse('1.2   mb')).toBe(1.2 * 1024 * 1024);
-
-    expect(parse('1.2 Gb')).toBe(1.2 * 1024 * 1024 * 1024);
-
-    expect(() => {
-      parse('1a.2 mb');
-    }).toThrow(`file size string '1a.2 mb' syntax error, e.g. 100 b, 1.2 Kb, 2 Mb, 20.5 Gb!`);
-  });
-
   test('format', () => {
     expect(format(100)).toBe('100.0 b');
     expect(format(10000)).toBe('9.8 Kb');
--- src/bytes.ts
+++ src/bytes.ts
@@ -1,23 +1,13 @@
-const JEDEC = { radix: 1024, unit: ['b', 'Kb', 'Mb', 'Gb'] };
+import parser from 'byte-parser';
 
-function unitToRate(unit: string) {
-  return unit === 'b' ? 1 : unit === 'kb' ? 1024 : unit === 'mb' ? 1024 * 1024 : unit === 'gb' ? 1024 * 1024 * 1024 : 1;
-}
+const JEDEC = { radix: 1024, unit: ['b', 'Kb', 'Mb', 'Gb'] };
 
 /**
  * 解析 size string
  * @param sizeString
  */
 export function parse(sizeString: string): number {
-  const match = sizeString.match(/^(\d*\.*\d*)\s*([gGkKmM]{0,1}[bB]{0,1})$/);
-  if (!match) {
-    throw new Error(`file size string '${sizeString}' syntax error, e.g. 100 b, 1.2 Kb, 2 Mb, 20.5 Gb!`);
-  }
-
-  const size = Number(match[1]);
-  const unit = match[2].toLowerCase();
-
-  return size * unitToRate(unit);
+  return parser(sizeString);
 }
 
 /**
